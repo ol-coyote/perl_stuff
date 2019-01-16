dd8e65cc3cbf91d2de27a18ea7b4cb68e48e884f
ol-coyote <you@you.com>
2019-01-16 11:46:08 -0500

    added example to output to 4_2

--- 4/4_2.pl
+++ 4/4_2.pl
@@ -10,3 +10,13 @@ while(<A>){
 	chomp(@lines);	
 	printf("%-16s\t%-6d\t%-6d\t%-12s\n",@lines[0],@lines[2],@lines[3],@lines[6]);
 }
+
+=pod
+% usage:
+$ perl 4_2.pl 
+LOGIN           	UID   	GUID  	SHELL       
+root            	0     	0     	/bin/bash   
+daemon          	1     	1     	/usr/sbin/nologin
+bin             	2     	2     	/usr/sbin/nologin
+sys             	3     	3     	/usr/sbin/nologin
+=cut
