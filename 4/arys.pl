53e4eb75cfff432f0c5a01e26304384534ca31f2
Darth Cat <ol-coyote@users.noreply.github.com>
2019-01-16 11:57:37 -0500

    updated list

--- 4/arys.pl
+++ 4/arys.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 
-my @names =(bob,steve,jane,dick,ash,bill,eva,eve,julia,kim,carmen,robert,will,ann,jill,chris,peter,perla,genesis,juan,david,phillip,lupe,amanda,denise);
+my @names =(kay,lisa,jessica,lynn,erin,melissa,victor,luis,bobby,bob,steve,jane,dick,ash,bill,eva,eve,julia,kim,carmen,robert,will,ann,jill,chris,peter,perla,genesis,juan,david,phillip,lupe,amanda,denise);
 my @sorted_names=sort(@names);
 print "@names\n";
 print "@sorted_names\n";
