64302612a57ec9b2909d8850a03815d912b3df0e
ol-coyote <you@you.com>
2019-01-17 08:43:40 -0500

    simple oneliner

--- shell/ish.pl
+++ shell/ish.pl
@@ -1,10 +1,5 @@
 #!/usr/bin/perl
 #
-# Small shell in perl
-# Hoping to use when I'm trying to break out
-# Dangerous AF: DO NOT USE on a production system!
-
-while(1){
-	chomp($code=<STDIN>);
-	eval(system($code));
-}
+# Try when landed in a non-interactive shell and trying to break out
+# 
+eval(system("/bin/sh"));
